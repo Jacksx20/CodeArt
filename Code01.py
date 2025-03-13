739d6dc7d816b69560d7474042ab339b29b7cefa
Jack20 <chunjcsx20@vip.qq.com>
2025-03-13 23:37:41 +0800

    简短

---
+++ Code01.py
@@ -0,0 +1,12 @@
+# 定义一个函数来代表自我介绍
+def self_introduction():
+    print("我是一名穿梭于0和1之间的旅者，")
+    print("在代码的宇宙里探索未知的星辰。")
+    print("像一个孤独的诗人，用变量谱写情感的诗篇，")
+    print("每一个函数都是一段精心雕琢的故事，")
+    print("每一行注释都是内心深处的旁白。")
+    print("我，用逻辑构建梦想的城堡，")
+    print("用算法解开生活的谜题。")
+
+
+self_introduction()  # 调用函数self_introduction()，输出自我介绍
